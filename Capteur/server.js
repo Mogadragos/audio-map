f8a87006f8d51df41d0831788dcef1979e9f0fd8
Mogador <jcaillaud17170@gmail.com>
2023-02-28 16:01:44 +0100

    Update capteur and models

--- Capteur/server.js
+++ Capteur/server.js
@@ -8,34 +8,55 @@ const clientFromConnectionString =
 // AMQP-specific factory function returns Client object from core package
 const client = clientFromConnectionString(connectionString);
 
+// use Message object from core package
+const Message = require("azure-iot-device").Message;
+
 const connectCallback = function (err) {
     if (err) {
         console.error("Could not connect: " + err);
     } else {
         console.log("Client connected");
+        try {
+            var msg = new Message(
+                JSON.stringify({
+                    device: "Device_1",
+                    position: { latitude: 0.0, longitude: 0.0 },
+                    decibels: 50,
+                })
+            );
+            client.sendEvent(msg, function (err) {
+                if (err) {
+                    console.log(err.toString());
+                } else {
+                    console.log("Message sent");
+                }
+            });
+        } catch (e) {
+            console.log(e);
+        }
 
-        client.getTwin(function (err, twin) {
-            if (err) {
-                console.error("could not get twin");
-            } else {
-                const patch = {
-                    position: {
-                        latitude: 0.0,
-                        longitude: 0.0,
-                    },
-                    decibels: 0.0,
-                };
+        // client.getTwin(function (err, twin) {
+        //     if (err) {
+        //         console.error("could not get twin");
+        //     } else {
+        //         const patch = {
+        //             position: {
+        //                 latitude: 0.0,
+        //                 longitude: 0.0,
+        //             },
+        //             decibels: 0.0,
+        //         };
 
-                twin.properties.reported.update(patch, function (err) {
-                    if (err) {
-                        console.error("could not update twin");
-                    } else {
-                        console.log("twin state reported");
-                        process.exit();
-                    }
-                });
-            }
-        });
+        //         twin.properties.reported.update(patch, function (err) {
+        //             if (err) {
+        //                 console.error("could not update twin");
+        //             } else {
+        //                 console.log("twin state reported");
+        //                 process.exit();
+        //             }
+        //         });
+        //     }
+        // });
     }
 };
 
